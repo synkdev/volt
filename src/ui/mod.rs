a5dae26667f631e1529cabcf06bc88ad1f045bba
Anant Narayan <anantnrg@gmail.com>
2024-01-23 18:12:47 +0530

    implement default colors

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -9,3 +9,17 @@ pub enum Color {
     Rgba(u32, u32, u32, f64),
     Rgb(u32, u32, u32),
 }
+
+impl Color {
+    pub fn default_hex() -> Self {
+        Color::Hex("#1e1d2d".to_string())
+    }
+
+    pub fn default_rgb() -> Self {
+        Color::Rgb(30, 29, 45)
+    }
+
+    pub fn default_rgba() -> Self {
+        Color::Rgba(30, 29, 45, 1.0)
+    }
+}

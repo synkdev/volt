d6105e3e95a3763299f32e015ba035182180b98d
Anant Narayan <anantnrg@gmail.com>
2024-02-03 21:30:53 +0530

    add trait methods

--- src/ui/layers.rs
+++ src/ui/layers.rs
@@ -6,6 +6,10 @@ use downcast_rs::{impl_downcast, Downcast};
 pub trait Layer: Downcast {
     fn render(&self, canvas: &skia::canvas::Canvas, paint: &mut skia::Paint);
     fn add(&mut self, component: dyn Component);
+    fn set_z_index(&mut self, z_index: usize);
+    fn get_bounds(&self) -> skia::Rect;
+    fn is_dirty(&self) -> bool;
+    fn set_dirty(&mut self, value: bool);
 }
 
 impl_downcast!(Layer);

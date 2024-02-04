31f5d8279c8fd491326c8b006fe8ee62c4c1bea6
Anant Narayan <anantnrg@gmail.com>
2024-02-04 22:25:15 +0530

    Cleanup

--- src/context/render.rs
+++ src/context/render.rs
@@ -16,40 +16,7 @@ impl Context {
             self.finish_render();
         }
     }
-    //
-    // pub fn find_dirty_components(&mut self) {
-    //     for (id, component) in self.components.iter_mut() {
-    //         if component.is_dirty() {
-    //             println!("dirty: {id}");
-    //             self.dirty_components.push(id.to_owned());
-    //         }
-    //     }
-    // }
-    //
-    // pub fn redraw_full(&mut self) {
-    //     let canvas = self.surface.surface.canvas();
-    //
-    //     canvas.clear(self.background);
-    //     for (_, component) in self.components.iter_mut() {
-    //         component.render(canvas, &mut self.paint);
-    //     }
-    //     self.dirty = false;
-    //     self.dirty_components.clear();
-    // }
-    //
-    // pub fn redraw_partial(&mut self) {
-    //     let canvas = self.surface.surface.canvas();
-    //
-    //     for dirty_component in &self.dirty_components {
-    //         println!("rendering: {dirty_component}");
-    //         let component: &mut Box<dyn Component> =
-    //             self.components.get_mut(dirty_component).unwrap();
-    //         component.render(canvas, &mut self.paint);
-    //         component.was_drawn();
-    //     }
-    //     self.dirty_components.clear();
-    // }
-    //
+
     pub fn finish_render(&mut self) {
         self.gr_context
             .gl_surface

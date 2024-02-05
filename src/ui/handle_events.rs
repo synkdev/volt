a624ebe2a10487f7166560313ab9320dfa0c65b5
Anant Narayan <anantnrg@gmail.com>
2024-02-05 21:37:12 +0530

    finish get_active_element

--- src/ui/handle_events.rs
+++ src/ui/handle_events.rs
@@ -8,10 +8,21 @@ pub(crate) enum MouseEventType {
 }
 
 pub(crate) fn get_active_element<'t>(
-    elements: &mut Vec<Box<dyn Element>>,
+    elements: &'t mut Vec<Box<dyn Element>>,
     active_element: Option<&mut Box<dyn Element>>,
     position: (f32, f32),
 ) -> (Option<&'t mut Box<dyn Element>>, MouseEventType) {
+    for element in elements.iter_mut() {
+        let bounds = element.get_bounds();
+        if bounds.contains(skia::Point::from(position)) {
+            return match active_element {
+                Some(curr_element) if std::ptr::eq(curr_element.as_ref(), element.as_ref()) => {
+                    (Some(element), MouseEventType::None)
+                }
+                _ => (Some(element), MouseEventType::Entered),
+            };
+        }
+    }
     match active_element {
         Some(_) => return (None, MouseEventType::Exited),
         None => return (None, MouseEventType::None),

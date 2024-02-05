1e0bcd5cf6ab2150e0a75f434c0025c8ec544eef
Anant Narayan <anantnrg@gmail.com>
2024-02-05 21:30:01 +0530

    start working on get_active_element rewrite

--- src/ui/handle_events.rs
+++ src/ui/handle_events.rs
@@ -1,7 +1,19 @@
+use crate::ui::element::Element;
 use skia::Contains;
 
 pub(crate) enum MouseEventType {
     Entered,
     Exited,
     None,
+}
+
+pub(crate) fn get_active_element<'t>(
+    elements: &mut Vec<Box<dyn Element>>,
+    active_element: Option<&mut Box<dyn Element>>,
+    position: (f32, f32),
+) -> (Option<&'t mut Box<dyn Element>>, MouseEventType) {
+    match active_element {
+        Some(_) => return (None, MouseEventType::Exited),
+        None => return (None, MouseEventType::None),
+    }
 }

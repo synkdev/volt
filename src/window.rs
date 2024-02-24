35c53eceacd5a7649eff99151f3f861b0188fbbd
Anant Narayan <anantnrg@gmail.com>
2024-02-24 10:48:20 +0530

    Create new function for winit window

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,10 @@
-use winit::window::Icon;
-
 use crate::color::Color;
+use std::sync::Arc;
+use winit::{
+    dpi::{LogicalPosition, LogicalSize},
+    event_loop::EventLoopWindowTarget,
+    window::{Icon, Window, WindowBuilder},
+};
 
 #[derive(Clone)]
 pub struct WindowOptions {
@@ -41,4 +45,40 @@ impl Default for WindowOptions {
             background: Color::Hex("#1e1d2d"),
         }
     }
+}
+
+pub fn new(event_loop: &EventLoopWindowTarget<()>, options: WindowOptions) -> Arc<Window> {
+    #[allow(unused_mut)]
+    let mut window_builder = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(options.size.0, options.size.1))
+        .with_blur(options.blur)
+        .with_title(options.title)
+        .with_active(options.active)
+        .with_visible(options.visible)
+        .with_transparent(options.transparent)
+        .with_blur(options.blur)
+        .with_maximized(options.maximized)
+        .with_resizable(options.resizable)
+        .with_decorations(options.decorations)
+        .with_position(LogicalPosition::new(options.position.0, options.position.1))
+        .with_window_icon(options.window_icon);
+
+    #[allow(dead_code)]
+    #[cfg(feature = "wayland")]
+    if let Some(app_id) = options.id {
+        use winit::platform::wayland::WindowBuilderExtWayland as _;
+        window_builder = window_builder.with_name(app_id, "");
+    }
+    #[allow(dead_code)]
+    #[cfg(feature = "x11")]
+    if let Some(app_id) = options.id {
+        use winit::platform::x11::WindowBuilderExtX11 as _;
+        window_builder = window_builder.with_name(app_id, "");
+    }
+
+    let window = window_builder
+        .build(event_loop)
+        .expect("Could not create a Winit window");
+
+    Arc::new(window)
 }

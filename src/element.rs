82ac64c2112764da63416f9e57c4b45873cd63f2
Anant Narayan <anantnrg@gmail.com>
2024-02-25 18:09:31 +0530

    Comment stuff out until i can compile

--- src/element.rs
+++ src/element.rs
@@ -4,8 +4,8 @@ use vello::Scene;
 pub trait Element {
 	fn render(&mut self, scene: &mut Scene);
 	fn taffy_id(&self) -> NodeId;
-	fn on_hover(&mut self);
-	fn on_click(&mut self);
-	fn on_right_click(&mut self);
-	fn dirty() -> bool;
+	// fn on_hover(&mut self);
+	// fn on_click(&mut self);
+	// fn on_right_click(&mut self);
+	// fn dirty() -> bool;
 }

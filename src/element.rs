a3ad5a9984afcb50b18e0dc4064bd569f09f005b
Anant Narayan <anantnrg@gmail.com>
2024-02-25 18:08:12 +0530

    On hover?

--- src/element.rs
+++ src/element.rs
@@ -4,6 +4,7 @@ use vello::Scene;
 pub trait Element {
 	fn render(&mut self, scene: &mut Scene);
 	fn taffy_id(&self) -> NodeId;
+	fn on_hover(&mut self);
 	fn on_click(&mut self);
 	fn on_right_click(&mut self);
 	fn dirty() -> bool;
